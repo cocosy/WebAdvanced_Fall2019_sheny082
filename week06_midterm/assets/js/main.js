722bf025ec3217a816915b815f8a06494308515e
Yang Shen <cocosuiban@gmail.com>
2019-10-14 23:59:33 -0400

    midterm

--- week06_midterm/assets/js/main.js
+++ week06_midterm/assets/js/main.js
@@ -12,17 +12,17 @@
 // });
 
 
-var $grid = $('.grid').packery({
-  itemSelector: '.grid-item',
-  columnWidth: 100
-});
+// var $grid = $('.grid').packery({
+//   itemSelector: '.grid-item',
+//   columnWidth: 100
+// });
 
-// make all grid-items draggable
-$grid.find('.grid-item').each( function( i, gridItem ) {
-  var draggie = new Draggabilly( gridItem );
-  // bind drag events to Packery
-  $grid.packery( 'bindDraggabillyEvents', draggie );
-});
+// // make all grid-items draggable
+// $grid.find('.grid-item').each( function( i, gridItem ) {
+//   var draggie = new Draggabilly( gridItem );
+//   // bind drag events to Packery
+//   $grid.packery( 'bindDraggabillyEvents', draggie );
+// });
 
 
 
